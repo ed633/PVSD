eed21c8cf1cd2fd923fd51be86143dc318353647
ed633 <edendecolli@icloud.com>
2018-10-09 10:41:12 -0400

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Student {
+    double GPA;
+    String name;
+
+    public Student(String studentName, double gradePointAverage) {
+        GPA = gradePointAverage;
+        name = studentName;
+
+    }
+}
